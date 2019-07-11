f77f27604e9090fb3a1024a18985ae225cd040f1
Winkel Yin <winkel515@hotmail.com>
2019-07-11 10:10:54 -0400

    Forgot to add quiz.dart file

---
+++ lib/quiz.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './answer.dart';
+import './question.dart';
+
+class Quiz extends StatelessWidget {
+  final String question;
+  final List answers;
+  final Function answerQuestion;
+
+  Quiz({
+    @required this.question,
+    @required this.answers,
+    @required this.answerQuestion,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(question),
+
+        // ... Operator takes things from a List and puts it within its parent List
+        ...(answers as List<Map>).map((answer) {
+          return Answer(answer['text'], answerQuestion);
+        }).toList()
+      ],
+    );
+  }
+}
